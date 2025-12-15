fba018696103e31c6d226b86863ea4f2967c5e30
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-12-15 21:17:22 +0300

    initialize commit

--- src/PatientList.java
+++ src/PatientList.java
@@ -1,41 +1,41 @@
-import java.lang.reflect.ParameterizedType;
 
 public class PatientList {
-    Patient head=null;
-    Patient tail=null;
-    Patient temp;
-    Patient temp2;
+    Patient head=null;  //First node of linked list
+    Patient tail=null;  //Last node of linked list
+    Patient temp;       //Used as current node
+    Patient temp2;      //Used as previous node
 
-    void addPatient(Patient p1){
+    void addPatient(Patient p1){    //Adds a new patient to the end of the list
 
-        if(head==null){
+        if(head==null){   //If list is empty,new patient becomes head and tail
             p1.next=null;
             head=p1;
             tail=p1;
             System.out.println("Patient registration list created");
-        }else{
+        }else{     //Add patient to the end using tail reference
             tail.next=p1;
             tail=p1;
             System.out.println(p1.id+" Patient id enregistered.");
         }
     }
-    void removePatient(int id) {
-        if (head == null) {
+    void removePatient(int id) {     //Removes a patient from the list by ID
+        if (head == null) {   //List is empty
             System.out.println("List is empty, no patient to delete ");
         } else {
-            if (id == head.id && head.next == null) {
-                head = null;//vb
+            if (id == head.id && head.next == null) {   //Only one node in the list
+                head = null;
                 tail = null;
-            } else if (id == head.id && head.next != null) {
+            } else if (id == head.id && head.next != null) {   // More than one node,move head forward
                 head = head.next;
                 System.out.println(id + " The patient with the ID was deleted");
-            } else {
-                temp = head;
-                temp2 = head;
+            } else {    //Deleting from middle or tail
+                temp2 = head;   //previous node
+                temp = head.next;  //current node
                 while (temp.next != null) {
-                    if (id == temp.id) {
+                    if (id == temp.id) {   //Remove current node
                         temp2.next = temp.next;
-                        System.out.println(id + "The patiTent with the ID was deleted");
+                        System.out.println(id + "The patient with the ID was deleted");
+                       // return;  //BAK BURAYAAA
                     }
                     temp2 = temp;
                     temp = temp.next;
@@ -43,12 +43,12 @@ public class PatientList {
                 if (id == temp.id) {
                     temp2.next = null;
                     tail = temp2;
-                    System.out.println(id + " The patiTent with the ID was deleted");
+                    System.out.println(id + " The patient with the ID was deleted");
                 }
             }
         }
     }
-     public boolean findPatient(int id){
+     public boolean findPatient(int id){  //Searches for patient by ID
         Patient temp=head;
         while(temp!=null){
             if(temp.id==id)
@@ -59,7 +59,7 @@ public class PatientList {
      }
 
 
-    void printList(){
+    void printList(){    //Prints all patients in the list
         if(head==null){
             System.out.println("List is empty");
         }else{
--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -65,6 +65,5 @@ public class TreatmentQueue {
         return cnt==size;
     }
 
-
 }
 
