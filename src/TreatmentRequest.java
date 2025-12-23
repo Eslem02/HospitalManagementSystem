3d668ef0d7457ef2936c0f1d4c1f977d81f28ff4
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-12-23 21:12:01 +0300

    initialize commit

--- src/TreatmentRequest.java
+++ src/TreatmentRequest.java
@@ -2,13 +2,12 @@ public class TreatmentRequest {
     int patientId;
     long arrivalTime;
     TreatmentRequest next;
-    boolean priority;
+
 
 
     public TreatmentRequest(int patientId, String name){
         this.patientId=patientId;
         this.arrivalTime=System.currentTimeMillis();
-        this.priority=priority;
         this.next=null;
     }
 }
